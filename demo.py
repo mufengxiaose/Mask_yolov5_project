50333bfd6c8df46aa7f3a86906dccf6fd598e436
Carl <xiaoliu911@126.com>
2022-03-29 18:21:19 +0800

    add mask train file

---
+++ demo.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+#@Time      :2022/3/2613:59
+#@Author    :Carl
+import torch
+
+# Model
+model = torch.hub.load('ultralytics/yolov5', 'yolov5s')  # or yolov5m, yolov5l, yolov5x, custom
+
+# Images
+img = 'https://ultralytics.com/images/zidane.jpg'  # or file, Path, PIL, OpenCV, numpy, list
+
+# Inference
+results = model(img)
+
+# Results
+results.print()  # or
